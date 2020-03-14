bdebd4bd28c7077aeaa113256671785013901c15
NaHyeongJin <56540533+NaHyeongJin@users.noreply.github.com>
2020-03-14 15:14:21 +0900

    Update Header.jsx

--- src/components/header-footer/Header.jsx
+++ src/components/header-footer/Header.jsx
@@ -3,12 +3,12 @@ import logo from '../../img/title.png';
 
 const Header = () => {
     return (
-        <>
+        <Fragment>
         <img class="header-image" src={logo} alt="Emory Logo"/>
         <div id="explanation">
            <b>How to use</b>
         </div>
-        </>
+        </Fragment>
     );
 };
 
