6dcf2acf436794310ed30640e9892492137267b2
Jinu Jang <jimjang95@gmail.com>
2020-03-19 22:08:45 -0400

    styled-components use, click to upload broken

---
+++ src/components/dragdrop/Dropzone.jsx
@@ -0,0 +1,52 @@
+import React, {useCallback} from 'react';
+import {useDropzone} from 'react-dropzone';
+import styled from 'styled-components';
+
+const Box = styled.div`
+  outline: 2px dashed #92b0b3;
+  background-color: #c8dadf;
+  outline-offset: -10px;
+  transition: outline-offset .15s ease-in-out, background-color .15s linear;
+  padding: 100px 20px;
+
+  p:hover b{
+    color: #39bfd3;
+  }
+`
+
+const BoxIcon = styled.svg`
+  width: 100%;
+  height: 80px;
+  fill: #92b0b3;
+  display: block;
+  margin-bottom: 40px;
+`
+
+function MyDropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+    console.log(acceptedFiles);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <Box {...getRootProps()}>
+      <BoxIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 43">
+        <path
+          d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z">
+        </path>
+      </BoxIcon>
+      <input {...getInputProps()}/>
+      {
+        isDragActive ?
+          <p>Drop it</p> :
+          <p><b>Choose a file</b> or drag it here</p>
+      }
+      <div className="box__uploading" _msthash="526214" _msttexthash="1845610" _msthidden="1">Uploading…</div>
+      <div className="box__success" _msthash="526617" _msttexthash="292370" _msthidden="1">Done! <a href="https://css-tricks.com/examples/DragAndDropFileUploading//?" className="box__restart" role="button">Upload more?</a></div>
+      <div className="box__error" _msthash="527020" _msttexthash="274079" _msthidden="1">Error! <span></span>. <a href="https://css-tricks.com/examples/DragAndDropFileUploading//?" className="box__restart" role="button">Try again!</a></div>
+    </Box>
+  )
+}
+
+export default MyDropzone;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./main.css";
 import Header from "./components/header-footer/Header";
+import Dropzone from "./components/dragdrop/Dropzone";
 import Footer from "./components/header-footer/Footer";
 
 
 ReactDOM.render(<Header />, document.getElementById("header"));
+ReactDOM.render(<Dropzone />, document.getElementById("dragdrop"));
 ReactDOM.render(<Footer />, document.getElementById("footer"));
